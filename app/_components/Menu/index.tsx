a3159f806d93d74f67ec9df637f93ca53263d394
kaito <nonokaisato@outlook.jp>
2025-07-22 16:40:47 +0900

    4-4-1

---
+++ app/_components/Menu/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./index.module.css";
+
+export default function Menu() {
+    return (
+        <div>
+            <nav className={styles.nav}>
+                <ul className={styles.items}>
+                    <li>
+                        <Link href="/news">ニュース</Link>
+                    </li>
+                    <li>
+                        <Link href="/members">メンバー</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">お問い合わせ</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
